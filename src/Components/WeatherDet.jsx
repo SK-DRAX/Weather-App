666b3b539740a97620a42b519d9ad25f80849e63
Swapnil Khot <74708262+SK-DRAX@users.noreply.github.com>
2021-08-03 11:56:44 +0530

    used useEffect

--- src/Components/WeatherDet.jsx
+++ src/Components/WeatherDet.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function WeatherDet() {
   // let apiKey = "daa2f095b0e458fd463d236f75e52a87";
-  // var fullDetails =
+  // var fullDetails ="api.openweathermap.org/data/2.5/weather?q=Mumbai&units=metric&appid=daa2f095b0e458fd463d236f75e52a87"
 
   const [items, setItems] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
-  function fetchData() {
+  useEffect(() => {
     fetch(
       "api.openweathermap.org/data/2.5/weather?q=Mumbai&units=metric&appid=daa2f095b0e458fd463d236f75e52a87"
     )
-      .then((resp) => (resp = resp.json()))
+      .then((res) => res.json())
       .then((result) => {
-        setLoaded(true);
+        setLoaded(false);
         setItems(result);
       });
-  }
+  }, []);
+
   console.log(items);
   return <h1> </h1>;
 }
